557cdf2a77f0935fcfae0f48f5595dd4f0e3dfbf
JGleslie017 <126440192+JGleslie017@users.noreply.github.com>
2023-03-17 21:48:54 -0600

    Add files via upload
    Desafíos en clase

---
+++ D.2/caja/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            namespace Superficie_frontal
+    {
+        class Caja
+        {
+            public int Alto { get; set; }
+            public int Largo { get; set; }
+
+            public int SuperficieFrontal
+            {
+                get
+                {
+                    return Alto * Largo;
+                }
+            }
+        }
+    }
+}
+    }
+}
---
+++ D.2/superficie frontal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace superficie_frontal
+{
+    internal class Program
+    {
+        class Caja
+        {
+            public int Alto { get; set; }
+            public int Largo { get; set; }
+
+            public int SuperficieFrontal
+            {
+                get
+                {
+                    return Alto * Largo;
+                }
+            }
+        }
+    }
+    
+        
+        
+    
+
